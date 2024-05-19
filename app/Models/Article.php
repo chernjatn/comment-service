4c63bc12cc16845862539afbd10029bad4bab138
Чернятин Андрей Игоревич <chernyatin.ai@erkapharm.com>
2024-05-19 20:05:36 +0300

    added the laravel-query-builder

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Article extends Model implements HasMedia
+class Article extends Model
 {
     use HasFactory, HasChannel;
 
