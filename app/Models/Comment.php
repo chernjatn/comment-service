70d1ce51efc7225bad562a2024a9381db40097c4
Чернятин Андрей Игоревич <chernyatin.ai@erkapharm.com>
2024-05-20 22:35:32 +0300

    added a scope active

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -20,5 +21,19 @@ class Comment extends Model
     public function article(): BelongsTo
     {
         return $this->belongsTo(Article::class, 'article_id','ext_id');
+    }
+
+    public function scopeActive(Builder $builder)
+    {
+        return $builder->where('is_active', true);
+    }
+
+    protected static function booted()
+    {
+        if (request()->wantsJson()) {
+            static::addGlobalScope('is_active', function (Builder $builder) {
+                $builder->active();
+            });
+        }
     }
 }
